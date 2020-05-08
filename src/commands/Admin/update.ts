8fdc4985cf782b204db4108e23b156e9cd8bebab
Caleb Blankemeyer <caleb@agrajog.com>
2020-05-08 02:01:17 -0400

    feat: update command to use cwd

--- src/commands/Admin/update.ts
+++ src/commands/Admin/update.ts
@@ -21,20 +21,20 @@ export default class extends Command {
     }
 
     private async compile(message: KlasaMessage) {
-        const {stderr, code} = await this.exec('cd ../../ && npm run build');
+        const {stderr, code} = await this.exec('npm run build');
         if (code !== 0 && stderr.length) throw stderr.trim();
         return message.channel.send(`:white_check_mark: Successfully compiled.`);
     }
 
     private async updateDependencies(message: KlasaMessage) {
-        const {stderr, code} = await this.exec('cd ../../ && npm ci');
+        const {stderr, code} = await this.exec('npm ci');
         if (code !== 0 && stderr.length) throw stderr.trim();
         return message.channel.send(`:white_check_mark: Successfully updated dependencies.`);
     }
 
     private async fetch(message: KlasaMessage, branch: string) {
         await this.exec('cd ../../ && git fetch');
-        const {stdout, stderr} = await exec(`cd ../../ && git pull origin ${branch}`);
+        const {stdout, stderr} = await exec(`git pull origin ${branch}`);
 
         // If it's up to date, do nothing
         if (/already up[ \-]to[ \-]date/i.test(stdout)) throw `:white_check_mark: Up to date.`;
@@ -57,7 +57,7 @@ export default class extends Command {
     private async stash(message: KlasaMessage) {
         await message.send('Unsuccessful pull, stashing...');
         await sleep(1000);
-        const {stdout, stderr} = await this.exec(`cd ../../ && git stash`);
+        const {stdout, stderr} = await this.exec(`git stash`);
         if (!this.isSuccessfulStash(stdout + stderr)) {
             throw `Unsuccessful pull, stashing:\n\n\`\`\`prolog\n${[stdout || '✔', stderr || '✔'].join('\n-=-=-=-\n')}\`\`\``;
         }
@@ -67,12 +67,12 @@ export default class extends Command {
 
     private async checkout(message: KlasaMessage, branch: string) {
         await message.send(`Switching to ${branch}...`);
-        await this.exec(`cd ../../ && git checkout ${branch}`);
+        await this.exec(`git checkout ${branch}`);
         return message.send(`:white_check_mark: Switched to ${branch}.`);
     }
 
     private async isCurrentBranch(branch: string) {
-        const {stdout} = await this.exec('cd ../../ && git symbolic-ref --short HEAD');
+        const {stdout} = await this.exec('git symbolic-ref --short HEAD');
         return stdout === `refs/heads/${branch}\n` || stdout === `${branch}\n`;
     }
 
@@ -90,7 +90,7 @@ export default class extends Command {
 
     private async exec(script: string) {
         try {
-            const result = await exec(script);
+            const result = await exec(script, { cwd: '../../' });
             return {...result, code: 0};
         } catch (error) {
             return {stdout: '', stderr: error.message || error || '', code: error.code};
