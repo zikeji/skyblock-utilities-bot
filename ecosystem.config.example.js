f5d4958fcef1fa34d78e769f09db348de8393baa
Caleb Blankemeyer <caleb@agrajog.com>
2020-05-07 21:15:59 -0400

    feat: add ecosystem example

---
+++ ecosystem.config.example.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'Skyblock Utilities',
+    script: 'dist/src/index.js',
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '512M',
+    env: {
+      NODE_ENV: 'production',
+      DB_HOST: '127.0.0.1',
+      DB_PORT: 5432,
+      DB_NAME: '',
+      DB_USER: '',
+      DB_PASSWORD: '',
+      REDIS_HOST: '127.0.0.1',
+      REDIS_PASSWORD: '',
+      REDIS_PORT: 6379,
+      REDIS_DB: 0,
+      BOT_TOKEN: '',
+      HYPIXEL_KEY: '',
+      COMMAND_LOGGING: 'yes'
+    }
+  }]
+};
