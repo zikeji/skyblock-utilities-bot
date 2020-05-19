b8b31b9a1494f4d51e7fa949a5eb48f35c77dc33
Zikeji <me@zikeji.com>
2020-05-18 21:16:30 -0400

    feat: reset shards to auto

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import {ShardingManager} from "discord.js";
 import {join} from 'path';
 
 const manager = new ShardingManager(join(__dirname, 'main.js'), {
-    token: process.env.BOT_TOKEN,
-    totalShards: 2
+    token: process.env.BOT_TOKEN
 });
 
 manager.spawn().catch(console.error);
